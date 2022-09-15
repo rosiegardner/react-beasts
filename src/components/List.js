f1464fb1bec2211feb9f7ae87742dba5a702ac6b
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-14 20:46:33 -0700

    WIP: click button

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Beast from './Beast';
 
 
 class List extends Component {
@@ -19,17 +20,25 @@ class List extends Component {
     .catch(error => console.log(error))
   }
 
+  // draw card -> one at a time -> randomly 
+  // handleDrawClick = () => {
+  //   {this.state.list.map((beast, i) => {
+  //     <div>
+  //       {beast}
+  //       onClick={() => this.setstate({ expanded: i })}
+  //       {this.state.expanded === i && (
+  //         <Beast beast={beast} key={beast.id} />
+  //       )}
+  //       </div>
+  //   })}
+  // }
+
   render () {
     return (
       <div>
-        {this.state.list.map((beast) => {
-          return(
-            <div className="tile" key ={beast.id} >
-              <h4>{beast.daemon}</h4>
-              <p>Breed: {beast.breed}</p>
-              <p>Name: {beast.name}</p>
-              <p>{beast.desc}</p>
-            </div>
+        {this.state.beasts.map((beast) => {
+          return (
+          <Beast beast={beast} key={beast.id} />
           )
         })}
       </div>
@@ -37,7 +46,48 @@ class List extends Component {
   }
 }
 
-
-
-
 export default List;
+
+{/* {this.state.list.map((beast, i) => {
+  <div>
+    {beast.name}
+    <button className='playCard' onClick={() => this.setState({ expanded: i })}>
+    HIT ME!
+    </button>
+    {this.state.expanded === i && (
+      <Beast key={beast.name} beast={beast} />
+    )}
+    </div>
+})} */}
+{/* <button className='playCard' onClick={this.handleDrawClick} >
+  HIT ME!
+</button> */}
+
+// const BeastInfo = ({ beast }) => {
+  //   return (
+    //     <ul>
+    //       {Object.entries(beast).map(([key, value]) => (
+      //         <li key={key}>{ `${key}: ${value}` }</li>
+      //       ))}
+      //     </ul>
+      //   );
+// };
+
+
+
+
+// render () {
+//   return (
+//     <div>
+//       {this.state.list.map((beast) => {
+//         return(
+//           <Beast beast={beast} key={beast.id} />
+//         )
+//       })}
+//       <button className='playCard' onClick={this.handleDrawClick} >
+//         90218!
+//       </button>
+//     </div>
+//   );
+// }
+// }

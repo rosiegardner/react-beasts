15530882852fe4f8fc17bce305750b8e542301ab
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-19 19:17:10 -0700

    import image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import daddysImage from "./../img/daddy2.png";
 
-function Header(props) {
+function Header() {
   return (
     <React.Fragment>
       <h1>BEASTS OF WAR</h1>
       <p>WELCOME TO HELL</p>
+      <img src={daddysImage} alt="shrek" />
+      <br></br>
+      <br></br>
     </React.Fragment>
   );
 }
